91809e2dab4ebfe69e83c75bf5a716fd0d20cfdd
Ifeoluwajayd1 <oluwajaydzie@gmail.com>
2021-10-10 23:31:56 +0100

    hello (php journey)

---
+++ hello.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <title>PHP Test</title>
+</head>
+<body>
+  <p>This is an HTML line
+    <?php
+    echo "<p>This is a PHP line</p>";
+    phpinfo();
+     ?>
+   </body>
+   </html>
